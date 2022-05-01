1759f145c53830db65049eed50800858f5a89f56
Jean-Philippe Moffet <jeanphilippemoffet@gmail.com>
2022-05-01 10:19:43 -0400

    Array.find réimplémenter

---
+++ array-filter.js
@@ -0,0 +1,12 @@
+const array = ['joe','4','45','mathieu' ];
+
+const newArray = [];
+
+for (let i = 0; i < array.length; i++){
+
+
+    if (array[i] == isNaN()) {
+        console.log("tes");
+      }
+  
+  }
---
+++ array-find.js
@@ -0,0 +1,16 @@
+
+  const array = ['John', 'jp', 46];
+const newArray = [];
+   
+   
+    for (let valeur of array) {
+
+        
+        valeur == 42 ? newArray.push(valeur):console.log("pas trouvé");
+        console.log(newArray);
+        
+     
+      }
+
+
+  
